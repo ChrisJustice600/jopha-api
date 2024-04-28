4afbe49f92ef418ab4c8e4ffc08fa158c17c2e6f
unknown <christianmabeladi@gmail.com>
2024-04-28 01:31:59 +0100

    create: jwtmodule auth

---
+++ config/jwtconfig.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+
+const generateToken = (user) => {
+  const payload = {
+    username: user.firstname,
+    id: user.id,
+    role: user.role,
+  };
+
+  // Generate token with secret key and expiration time (48 hours)
+  return jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: Date.now() + 48 * 60 * 60 * 1000,
+  });
+};
+
+const verifyToken = (token) => {
+  try {
+    // Verify token using secret key
+    return jwt.verify(token, process.env.JWT_SECRET);
+  } catch (err) {
+    return null;
+  }
+};
+
+module.exports = {
+  generateToken,
+  verifyToken,
+};
