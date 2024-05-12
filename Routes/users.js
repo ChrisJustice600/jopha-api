855023cb2cd743b5c5397fcdbe37d8908b8bd296
unknown <christianmabeladi@gmail.com>
2024-05-12 20:19:32 +0100

    feat: recup each project associé aux user

--- Routes/users.js
+++ Routes/users.js
@@ -1,16 +1,19 @@
 const { Router } = require("express");
-const { getProfile, createProject } = require("../controllers/users");
+const {
+  getProfile,
+  createProject,
+  getAllUserProjects,
+} = require("../controllers/users");
 const { checkUserRole } = require("../middlewares/checkUserRole");
 const {
   checkUserAuthenticated,
 } = require("../middlewares/checkUserAuthenticated");
 const path = require("path");
-
 const multer = require("multer");
 const upload = multer({
   dest: "uploads/", // Set your upload destination directory
   fileFilter: (req, file, cb) => {
-    const allowedExtensions = [".jpg", ".jpeg", ".png"]; // Allowed file types
+    const allowedExtensions = [".jpg", ".jpeg", ".png", ".webp"]; // Allowed file types
     const extname = path.extname(file.originalname);
     if (allowedExtensions.includes(extname)) {
       cb(null, true);
@@ -23,11 +26,8 @@ const upload = multer({
 const usersRouter = Router();
 
 // Pour accéder aux infos de profile on vérifie si le user est authentifié et si il a le rôle USER
-usersRouter.get("/profile", checkUserAuthenticated, checkUserRole, getProfile);
-usersRouter.post(
-  "/create/project",
-  upload.single("file"),
-  createProject
-);
+usersRouter.get("/profile", checkUserAuthenticated, getProfile);
+usersRouter.post("/create/project", upload.single("file"), createProject);
+usersRouter.get("/v/project", getAllUserProjects);
 
 module.exports = usersRouter;
--- controllers/users.js
+++ controllers/users.js
@@ -7,6 +7,25 @@ const getProfile = (req, res) => {
   const isTokenValide = verifyToken(token);
   res.status(200).json(isTokenValide);
 };
+const getAllUserProjects = async (req, res) => {
+  try {
+    const projects = await prisma.project.findMany({
+      include: {
+        user: {
+          select: {
+            username: true,
+          },
+        },
+      },
+    });
+    res.json(projects);
+  } catch (error) {
+    res.status(500).json({
+      error:
+        "Une erreur est survenue lors de la récupération des utilisateurs associés aux projets.",
+    });
+  }
+};
 
 const createProject = async (req, res) => {
   const { token } = req.cookies;
@@ -17,27 +36,31 @@ const createProject = async (req, res) => {
     const ext = parts[parts.length - 1];
     const newPath = path + "." + ext;
     console.log(newPath);
-    // Optional: Perform file operations or store newPath in database
-    // fs.renameSync(path, newPath); // This line can be used to rename the file
+
     const { token } = req.cookies;
     const isTokenValide = verifyToken(token);
     const { titre, objectif, categorie, content } = req.body;
 
-    const user = await prisma.project.create({
-      data: {
-        titre,
-        description: content,
-        photo: newPath,
-        objectif,
-        categorie,
-        userId: isTokenValide.id,
-      },
-    });
-    console.log(user);
+    try {
+      const userProject = await prisma.project.create({
+        data: {
+          titre,
+          description: content,
+          photo: newPath,
+          objectif,
+          categorie,
+          userId: isTokenValide.id,
+        },
+      });
+      res.status(200).json(userProject);
+    } catch (error) {
+      console.error(`Error creating record in ${modelName}:`, error);
+      return null;
+    }
   } catch (error) {
     console.error("Error uploading file:", error);
     res.status(500).json({ message: "Error uploading file" });
   }
 };
 
-module.exports = { getProfile, createProject };
+module.exports = { getProfile, createProject, getAllUserProjects };
