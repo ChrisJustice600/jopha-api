e926bb932d3fd193455659570e7ebb322f41fdda
unknown <christianmabeladi@gmail.com>
2024-05-21 16:03:10 +0100

    fix cors policy for vercel domain

--- index.js
+++ index.js
@@ -11,7 +11,13 @@ const authRouter = require("./Routes/auth");
 const usersRouter = require("./Routes/users");
 const { verifyToken } = require("./config/jwtconfig");
 // app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
-app.use(cors({ credentials: true, origin: "https://impact-ten.vercel.app//" }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://impact-ten.vercel.app");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.use(cookieParser());
 
