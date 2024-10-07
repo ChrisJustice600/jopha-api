f835d9a8854b37b3f4eff48e6fb7cb58bbc8ce9f
unknown <christianmabeladi@gmail.com>
2024-10-07 11:35:32 +0100

    reinstall dependency

--- prisma/migrations/20241003152435_delete_field_nom_agence_in_model_client_avec_code/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `nomAgence` on the `ClientAvecCode` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "ClientAvecCode" DROP COLUMN "nomAgence";
