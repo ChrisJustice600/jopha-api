7c3de62e318980729fbc3915182c97f354b5f58f
christian mabeladi <christianmabeladi@gmail.com>
2025-01-18 18:22:23 +0100

    feat: logout controller

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,6 +5,7 @@ const authRouter = Router();
 
 authRouter.post("/register", authController.register);
 authRouter.post("/signin", authController.signin);
+authRouter.post("/logout", authController.logout);
 authRouter.post("/forgot-password", authController.forgotPassword);
 authRouter.post("/reset-password", authController.resetPassword);
 // authRouter.post(`/signup/:adminRouteParams`, signup);
